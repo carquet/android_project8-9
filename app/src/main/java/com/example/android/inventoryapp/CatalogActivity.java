ac6623b13044eacbac59a0fbdee1cc38bcc3dfdd
L.Carquet <laetitiacarquet@gmail.com>
2018-07-21 12:03:29 +0200

    adds a contrc¡act and a db helper

--- app/src/main/java/com/example/android/inventoryapp/CatalogActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/CatalogActivity.java
@@ -2,6 +2,7 @@ package com.example.android.inventoryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CatalogActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class CatalogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        TextView bookView = (TextView) findViewById(R.id.text_view_books);
+        bookView.append("YOUPI");
     }
 }
---
+++ app/src/main/java/com/example/android/inventoryapp/data/BookContract.java
@@ -0,0 +1,27 @@
+package com.example.android.inventoryapp.data;
+
+import android.provider.BaseColumns;
+
+//this should be in a data package inside the app package
+public final class BookContract {
+
+    //CREATE TABLE books(product_name, price, in_stock,quantity,supplier_name,supplier_phone_number)
+
+    public static abstract class BookEntry implements BaseColumns {
+      
+        public static final String TABLE_NAME = "books";
+        public static final String COLUMN__PRODUCT_NAME = "product_name";
+        public static final String COLUMN_PRICE = "price";
+        public static final String COLUMN_IN_STOCK = "in_stock";
+        public static final String COLUMN_QUANTITY = "quantity";
+        public static final String COLUMN_SUPPLIER_NAME = "supplier_name";
+        public static final String COLUMN_SUPPLIER_PHONE_NUMBER = "supplier_phone_number";
+    
+        /**
+         * Possible values for the product being in stock.
+         */
+        public static final int IN_STOCK = 0;
+        public static final int NOT_IN_STOCK = 1;
+
+    }
+}
